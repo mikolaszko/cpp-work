65a8700881e011fdbde466e19f2244bf8462a65a
mikolaszko <mikosodz.lote@gmail.com>
2023-10-26 16:45:51 +0200

    added comment for the implicit type conversion in 4.12

--- 4/4.12.cpp
+++ 4/4.12.cpp
@@ -5,6 +5,11 @@ int main()
     std::cout << "Enter a single character ";
     char c{};
     std::cin >> c;
+    /*
+        this is an implicit type conversion which is NOT recommened but can occur,
+        the same would happen if we passed c to function: int convertCharToInt(char c)
+        because return type is int OR int convertCharToIntImmediately(int c) because of the parameter type
+    */
     int ascii{c};
 
     std::cout << "You have entered '" << c << "' which is " << ascii << " in ascii but can also be converted statically like this " << static_cast<int>(c) << "\n";
